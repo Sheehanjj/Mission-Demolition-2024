17ecf0f15dd81096c6a5fc67cafc270b5a7a3a37
John Sheehan <sheehanj@sunyulster.edu>
2024-03-26 14:21:37 -0400

    Version 1.0 - 3-26-24 Class

---
+++ Assets/Slingshot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+	void onMouseEnter(){
+		print("Slingshot:OnMouseEnter()");
+	}
+	
+	void onMouseExit(){
+		print("Slingshot:OnMouseExit()");
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
